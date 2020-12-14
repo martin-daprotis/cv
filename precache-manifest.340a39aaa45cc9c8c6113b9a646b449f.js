ed906eed0cf876446652b142bfb0cad7562523e9
mdaprotis-bg <martin.daprotis@bglobalsolutions.com>
2020-12-14 09:59:53 -0300

    Updates

---
+++ precache-manifest.340a39aaa45cc9c8c6113b9a646b449f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40da3146d288afc42c1c081086a004f6",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "6404dfb3870933b3d080",
+    "url": "/cv/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "6404dfb3870933b3d080",
+    "url": "/cv/static/js/2.b22c2e9e.chunk.js"
+  },
+  {
+    "revision": "a9f95430e381cb1f80f828c6be71b062",
+    "url": "/cv/static/js/2.b22c2e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a619e65ab44de82d3d",
+    "url": "/cv/static/js/main.8cb28741.chunk.js"
+  },
+  {
+    "revision": "7e40de526988cdea935f",
+    "url": "/cv/static/js/runtime-main.a7e27023.js"
+  },
+  {
+    "revision": "52d9f87b93f862bc9471a0b096d631ce",
+    "url": "/cv/static/media/keyboard.52d9f87b.jpg"
+  },
+  {
+    "revision": "f374d503d667532510754e5820b2a90f",
+    "url": "/cv/static/media/pic4.f374d503.png"
+  }
+]);
