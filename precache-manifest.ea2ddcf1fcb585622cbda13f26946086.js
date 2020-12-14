5f0d7b738ff30077755ced9689b22d4343d6d089
mdaprotis-bg <martin.daprotis@bglobalsolutions.com>
2020-12-14 14:54:35 -0300

    Updates

--- precache-manifest.ea2ddcf1fcb585622cbda13f26946086.js
+++ precache-manifest.ea2ddcf1fcb585622cbda13f26946086.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40da3146d288afc42c1c081086a004f6",
+    "revision": "115a2bf3a4fffd7e1cd0a5256dc61fab",
     "url": "/cv/index.html"
   },
   {
-    "revision": "6404dfb3870933b3d080",
+    "revision": "8a5429df5b5b2f4941b3",
     "url": "/cv/static/css/2.09ae8418.chunk.css"
   },
   {
-    "revision": "6404dfb3870933b3d080",
-    "url": "/cv/static/js/2.b22c2e9e.chunk.js"
+    "revision": "8a5429df5b5b2f4941b3",
+    "url": "/cv/static/js/2.4cbdcdc2.chunk.js"
   },
   {
-    "revision": "a9f95430e381cb1f80f828c6be71b062",
-    "url": "/cv/static/js/2.b22c2e9e.chunk.js.LICENSE.txt"
+    "revision": "60413b93963bd3a6142050d93c2cf18d",
+    "url": "/cv/static/js/2.4cbdcdc2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80a619e65ab44de82d3d",
-    "url": "/cv/static/js/main.8cb28741.chunk.js"
+    "revision": "fca5d6ff30083be2ebd2",
+    "url": "/cv/static/js/main.9984ae0a.chunk.js"
   },
   {
     "revision": "7e40de526988cdea935f",
     "url": "/cv/static/js/runtime-main.a7e27023.js"
+  },
+  {
+    "revision": "721d4c83f3e33d3860f5a9934e2e09c3",
+    "url": "/cv/static/media/CV_MARTIN_DAPROTIS.721d4c83.pdf"
   },
   {
     "revision": "52d9f87b93f862bc9471a0b096d631ce",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.340a39aaa45cc9c8c6113b9a646b449f.js"
+  "/cv/precache-manifest.ea2ddcf1fcb585622cbda13f26946086.js"
 );
 
 self.addEventListener('message', (event) => {
